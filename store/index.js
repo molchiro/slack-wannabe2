d92d1ae696433c1ae0ab83abf8fdbc9237839e7f
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-10-13 00:53:27 +0900

    beautify

--- store/index.js
+++ store/index.js
@@ -1,37 +1,38 @@
 import Vuex from 'vuex'
 
-const store = () => new Vuex.Store({
-  state: {
-    auth: {
-      isLoaded: true,
-      user: null,
+const store = () =>
+  new Vuex.Store({
+    state: {
+      auth: {
+        isLoaded: true,
+        user: null,
+      },
     },
-  },
-  mutations: {
-    setUser(state, user) {
-      state.auth.user = user
+    mutations: {
+      setUser(state, user) {
+        state.auth.user = user
+      },
+      loaded(state, status) {
+        state.auth.isLoading = false
+      },
     },
-    loaded(state, status) {
-      state.auth.isLoading = false
+    getters: {
+      isAuthed: (state) => {
+        return state.auth.user !== null
+      },
+      isLoaded: (state) => {
+        return state.auth.isLoaded
+      },
+      user: (state) => {
+        return state.auth.user
+      },
     },
-  },
-  getters: {
-    isAuthed: (state) => {
-      return state.auth.user !== null
+    actions: {
+      AuthStateChanged(context, user){
+        context.commit('loaded')
+        context.commit('setUser', user)
+      },
     },
-    isLoaded: (state) => {
-      return state.auth.isLoaded
-    },
-    user: (state) => {
-      return state.auth.user
-    },
-  },
-  actions: {
-    AuthStateChanged(context, user){
-      context.commit('loaded')
-      context.commit('setUser', user)
-    }
-  }
-})
+  })
 
 export default store
