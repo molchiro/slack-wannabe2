0315b8c4364ed7399a6a77304ee58efbd29de412
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-10-15 00:02:12 +0900

    displayNameとtimestampの表示 contentの改行の扱いには失敗してる

--- components/MessageItem.vue
+++ components/MessageItem.vue
@@ -1,12 +1,24 @@
 <template lang="pug">
   .container
     .message-item
-    |{{ message.val.displayName }}
-    |{{ message.val.content }}
+    | {{ message.val.displayName}}
+    | {{ message.val.timestamp | formatUNIXtime }}
+    br
+    | {{ message.val.content | formatNewLine }}
 </template>
 
 <script>
+import format from 'date-fns/format'
+
 export default {
+  filters: {
+    formatUNIXtime(UNIXtime) {
+      return format(UNIXtime, 'YYYY-MM-DD HH:mm')
+    },
+    formatNewLine(str) {
+      return str.replace(/\n/g, '<br>')
+    },
+  },
   props: {
     message: Object,
   },
