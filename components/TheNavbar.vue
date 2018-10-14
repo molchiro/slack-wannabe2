996a10c336a7324b5d383fa99700c293be7ec7ee
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-10-14 23:11:37 +0900

    sign out

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -2,9 +2,21 @@
   .container
     ul
       li.app-title slack-wannabe2
-      li.button sign out
+      li.button(@click="signOut") sign out
       li.auth-username molchiro
 </template>
+
+<script>
+import firebase from '~/plugins/firebase.js'
+
+export default {
+  methods: {
+    signOut() {
+      firebase.auth().signOut()
+    },
+  },
+}
+</script>
 
 <style scoped lang="sass">
   .container
