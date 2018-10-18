303d0f6d69dbc79bb84bfc655133f676d30a6676
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-10-18 23:21:27 +0900

    make vuex module "messages"

--- store/index.js
+++ store/index.js
@@ -1,10 +1,13 @@
 import Vuex from 'vuex'
 import auth from './modules/auth'
+import messages from './modules/messages'
+
 
 const store = () =>
   new Vuex.Store({
     modules: {
       auth,
+      messages,
     },
   })
 
