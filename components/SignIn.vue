8e6f2c56199f0a74e021f58119d7a50466565ab3
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-10-18 23:09:56 +0900

    ボタンにloading表示機能

--- components/SignIn.vue
+++ components/SignIn.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
   .container
-    button.button(@click="signIn") sign in
+    button.button.is-primary(
+      @click="signIn"
+      :class="{ 'is-loading': !isLoaded }"
+    ) sign in
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import firebase from '~/plugins/firebase.js'
 const provider = new firebase.auth.GoogleAuthProvider()
 
 export default {
+  computed: mapState('auth', ['isLoaded']),
   methods: {
     signIn() {
       firebase.auth().signInWithRedirect(provider)
