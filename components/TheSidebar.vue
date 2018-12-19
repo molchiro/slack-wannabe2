978c28642fae40550449e77f5257e5104dba75d8
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-12-19 22:23:52 +0900

    sidebarを作成してrooms表示

---
+++ components/TheSidebar.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  v-navigation-drawer
+    v-list
+      v-list-tile(
+        v-for="room in rooms"
+        :key="room.id"
+        @click=""
+      )
+        v-list-tile-content
+          v-list-tile-title {{ room.name }}
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState('rooms', ['rooms']),
+  mounted() {
+    this.$store.dispatch('rooms/initRooms')
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template lang="pug">
-  v-layout(column)
-    MessageList.scroll-y(style="height: calc(90vh - 64px)")
-    MessagePost(style="height: 10vh")
+  v-layout
+    TheSidebar
+    v-layout(column)
+      MessageList.scroll-y(style="height: calc(90vh - 64px)")
+      MessagePost(style="height: 10vh")
 </template>
 
 <script>
 import MessageList from '~/components/MessageList'
 import MessagePost from '~/components/MessagePost'
+import TheSidebar from '~/components/TheSidebar'
 
 export default {
   components: {
     MessageList,
     MessagePost,
+    TheSidebar,
   },
 }
 </script>
