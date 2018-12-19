1f7e53a200a99631e3889e7469db5bc66d12728b
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-12-19 22:05:57 +0900

    roomsの中身をfirestoreから取ってくる

--- store/modules/rooms.js
+++ store/modules/rooms.js
@@ -1,9 +1,35 @@
+import firebase from '~/plugins/firebase.js'
+const db = firebase.firestore()
+db.settings({ timestampsInSnapshots: true })
+const roomsRef = db.collection('rooms')
+
 export default {
   namespaced: true,
   state() {
     return {
-      rooms: ['7GgevlydJpzgema5UCJB'],
+      rooms: [],
       selectedRoom: '7GgevlydJpzgema5UCJB',
     }
+  },
+  mutations: {
+    initRooms(state) {
+      state.rooms = []
+    },
+    addRoom(state, roomID) {
+      state.rooms.push(roomID)
+    },
+  },
+  actions: {
+    initRooms({ commit, rootState }) {
+      commit('initRooms')
+      roomsRef
+        .where('members', 'array-contains', rootState.auth.authedUser.uid)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            commit('addRoom', { ...doc.data(), id: doc.id })
+          })
+        })
+    },
   },
 }
