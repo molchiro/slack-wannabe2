5dd05729a21b1e4e05436cb8be65e07dd3f44e51
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2018-12-19 23:37:46 +0900

    messageItemの削除ボタンが、自分のmessageにだけ表示されるように

--- components/MessageItem.vue
+++ components/MessageItem.vue
@@ -8,7 +8,7 @@
       v-list-tile-sub-title.text--primary(v-html="formatNewLine(message.data.content)" )
     v-list-tile-action
       v-list-tile-action-text {{ message.data.timestamp | formatUNIXtime }}
-      v-btn(@click='deleteMessage') 削除
+      v-btn(v-if='message.data.uid === authedUser.uid' @click='deleteMessage') 削除
 </template>
 
 <script>
